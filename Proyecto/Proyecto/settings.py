fa59378b21888ce1d63ffdbacf3c46149b2afa3a
VictorNaveros <analisisydesarrollodesoftwar1e@gmail.com>
2026-02-25 00:23:02 -0500

    Prueba 2

--- Proyecto/Proyecto/settings.py
+++ Proyecto/Proyecto/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-#2g-(hxyk-au7v!x!-1f5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
 
 # Application definition
 
