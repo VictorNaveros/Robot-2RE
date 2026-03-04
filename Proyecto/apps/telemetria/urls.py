5bb189049339449d85509224553129ac0ddf51eb
VictorNaveros <analisisydesarrollodesoftwar1e@gmail.com>
2026-03-04 14:37:39 -0500

    Urls

--- Proyecto/apps/telemetria/urls.py
+++ Proyecto/apps/telemetria/urls.py
@@ -4,6 +4,6 @@ from .views import esperar_orden_servo, recibir_telemetria
 app_name = 'telemetria'
 
 urlpatterns = [
-    path("telemetria/", recibir_telemetria, name='recibir_telemetria'),
+    path("", recibir_telemetria, name='recibir_telemetria'),
     path('esperar-orden-servo/', esperar_orden_servo, name='esperar_orden_servo'),
 ]
